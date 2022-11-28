3f52c467ebda25a191af3b877780ec9c6eba4648
damonfdo <damonfdo@live.com>
2022-11-28 12:52:12 +0530

    Modified Cors in Server

--- server/app.js
+++ server/app.js
@@ -2,6 +2,8 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
 
+// Import cors 
+import corsOptions from './Config/corsOption.js'
 // Fetch custom route files 
 import userRoutes from './Routes/User.routes.js'
 import taskRoutes from './Routes/Task.routes.js'
@@ -12,7 +14,7 @@ const app = express()
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
-app.use(cors())
+app.use(cors(corsOptions))
 
 // Add Routes to express app 
 app.use('/user', userRoutes)
