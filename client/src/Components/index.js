4dc6d8b81a592a5c07fbe22920f148e44b7fb8a1
damonfdo <damonfdo@live.com>
2022-11-29 16:27:13 +0530

    Component Index File

--- client/src/Components/index.js
+++ client/src/Components/index.js
@@ -2,10 +2,12 @@ import Header from "./Header.jsx"
 import Auth from "./Auth/Authentication"
 import Nav from "./Nav.jsx"
 import TaskList from "./TaskList.jsx"
+import CreateTask from "./CreateTask.jsx"
 
 export {
     Header,
     Auth,
     Nav,
-    TaskList
+    TaskList,
+    CreateTask
 }
